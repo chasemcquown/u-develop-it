24ea014e625f3a0713293276ee6663276f46c93d
chasem <cpmcquown@gmail.com>
2021-07-19 17:20:50 -0700

    adds connection to database

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// connect to the MySQL database
+const mysql = require('mysql2');
+
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,6 +10,24 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// the folllowing code will add connect the application to the MySQL database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'PeanutButterRottweiler&22',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// query the database to test the connection. NOTE: in the following code, the db object is using the query() method. This method runs the SQL query and executes the callback with all the resulting rows that match the query . Once this method executes the SQL command, the callback function captures the responses from the query in two variables: the err, which is the error response, and rows, which is the database query response.
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // add route to handle user requests that aren't supported by the app
 // Default response for any other request (Not Found)
