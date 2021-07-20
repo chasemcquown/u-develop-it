5c1a146147710a8eb5c62914a07475b52e571a6f
chasem <cpmcquown@gmail.com>
2021-07-19 18:20:02 -0700

    add CRUD routes

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const mysql = require('mysql2');
 
 const express = require('express');
 
+// inport the inputCheck method so we can use the function
+const inputCheck = require('./utils/inputCheck');
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -25,40 +28,87 @@ const db = mysql.createConnection(
 );
 
 // query the database to test the connection. NOTE: in the following code, the db object is using the query() method. This method runs the SQL query and executes the callback with all the resulting rows that match the query . Once this method executes the SQL command, the callback function captures the responses from the query in two variables: the err, which is the error response, and rows, which is the database query response.
-/* db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-}); */
+// GET all candidates, wrap in an Express.js route
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+});
 
 // retrieve a candidate based on their unique ID #
 // GET a single candidate
-// GET a single candidate
-/* db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log(row);
-}); */
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+});
 
 // add the ability to delete a candidate
 // Delete a candidate
-/* db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log(result);
-}); */
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.statusMessage(400).json({ error: res.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Candidate not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+});
 
 // add the ability to creat a candidate
 // Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-              VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
+// NOTE: notice that we're using object destructuring to pull the body property out of the request object, rather than it being req.body
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+    // NOTE:  there is no column for the id. MySQL will autogenerate the id 
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
 
-db.query(sql, params, (err, result) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(result);
+    db.query(sql, params, (err, result) => {
+        if (err) {
+         res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+         });
+    });
 });
 
 // add route to handle user requests that aren't supported by the app
